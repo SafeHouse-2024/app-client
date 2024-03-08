df518539f7a056ce616c36133d3994f922bda8c4
maarcos-floriano <marcos.floriano@sptech.school>
2024-03-08 20:08:47 -0300

    adicionei comentarios para cada metodo

--- src/main/java/org/app/client/util/driver/DriverManagerWindows.java
+++ src/main/java/org/app/client/util/driver/DriverManagerWindows.java
@@ -11,33 +11,38 @@ import java.util.stream.Collectors;
 
 public class DriverManagerWindows {
 
-    private static Map<String, Boolean> driverPermitidos = new HashMap<>();
+    private static Map<String, Boolean> driverPermitidos = new HashMap<>(); // Mapa para armazenar os drivers permitidos
 
+    // Verifica se o driver existe
     private static boolean verificarDriver(String nomeDriver){
         return new File(nomeDriver).exists();
     }
 
+    // Atualiza a lista de drivers conectados
     private static void atualizarDriversConectados(){
         for (int i = 0; i <= 25; i++){
             driverPermitidos.put(String.valueOf((char)('A' + i)), verificarDriver( (char)('A' + i) + ":/"));
         }
     }
 
+    // Mostra os drivers permitidos
     public static void mostrarDrivers(){
         for (int i = 0; i <= 25; i++) {
             System.out.println(driverPermitidos.get(String.valueOf((char)('A' + i))));
         }
     }
 
+    // Remove os drivers inválidos
     public static void removerDriversInvalidos(){
         driversInvalidos().forEach(drive -> PowerShell.executeSingleCommand("(New-Object -comObject Shell.Application).Namespace(17).ParseName(\"%s:\").InvokeVerb(\"Eject\")".formatted(drive)));
     }
 
+    // Retorna uma lista de drivers inválidos
     private static List<String> driversInvalidos(){
         atualizarDriversConectados();
         // Drivers que virão do banco de dados;
-        List<String> driversValidos = List.of("C");
-        List<String> driversExistentes = new ArrayList<>();
+        List<String> driversValidos = List.of("C"); // Lista de drivers válidos
+        List<String> driversExistentes = new ArrayList<>(); // Lista de drivers existentes
         driverPermitidos.forEach((key, value) -> {
             if (value) {
                 driversExistentes.add(key);
