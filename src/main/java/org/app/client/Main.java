ad1062e99e4bf564e5b1c4428008a2db0f9c6121
maarcos-floriano <marcos.floriano@sptech.school>
2024-04-03 17:37:06 -0300

    adicionei informações do sistema ao app

--- src/main/java/org/app/client/Main.java
+++ src/main/java/org/app/client/Main.java
@@ -1,25 +1,67 @@
 package org.app.client;
 
+import java.util.Scanner;
+
 import org.app.client.login.Login;
 import org.app.client.util.ExecutarPrograma;
-import org.app.client.util.driver.DriverManagerLinux;
-import org.app.client.util.driver.DriverManagerWindows;
-import org.app.client.util.tasks.TaskManager;
+
+import com.github.britooo.looca.api.core.Looca;
 
 
 
 public class Main {
     public static void main(String[] args) {
+        Looca looca = new Looca();
+        Scanner scanner = new Scanner(System.in);
         Login.autenticar();
         String sistemaOperacional = System.getProperty("os.name");
         String user = sistemaOperacional.contains("nux") ? System.getProperty("user.name") : null;
-        while(true){
-            ExecutarPrograma.executarPrograma(sistemaOperacional, user);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+
+        System.out.println("Vamos passar informações do seu computador ->");
+        // String sistemaOperacional = looca.getSistema().getSistemaOperacional();
+        String processador = looca.getProcessador().getNome();
+        String memoriaRam = looca.getMemoria().getTotal().toString();
+
+        System.out.println("Sistema Operacional: " + sistemaOperacional);
+        System.out.println("Processador: " + processador);
+        System.out.println("Memória RAM: " + memoriaRam);
+
+        System.out.println("Vamos passar informações do seu computador <-");
+
+        System.out.println("Você quer ver mais detalhes sobre cada item? (S/N)");
+        String detalhes = scanner.nextLine();
+
+        while (detalhes != null && detalhes.equalsIgnoreCase("S")) {
+            System.out.println("Qual item você quer ver mais detalhes? (Sistema Operacional, Processador, Memória RAM)");
+            String item = scanner.nextLine();
+
+            if (item.equalsIgnoreCase("Sistema Operacional")) {
+                System.out.println("Sistema Operacional: " + looca.getSistema());
+            } else if (item.equalsIgnoreCase("Processador")) {
+                System.out.println("Processador: " + looca.getProcessador());
+            } else if (item.equalsIgnoreCase("Memoria RAM")) {
+                System.out.println("Memória RAM: " + looca.getMemoria());
+            } else {
+                System.out.println("Item não encontrado");
             }
+
+            System.out.println("Você quer ver mais detalhes sobre cada item? (S/N)");
+            detalhes = scanner.nextLine();
+            
         }
+        System.out.println("Fim da execução");
+
+        System.out.println("Vamos agora verificar se há tarefas e drivers inválidos para serem removidos");
+        ExecutarPrograma.executarPrograma(sistemaOperacional, user);
+        scanner.close();
+
+        // while(true){
+        //     ExecutarPrograma.executarPrograma(sistemaOperacional, user);
+        //     try {
+        //         Thread.sleep(5000);
+        //     } catch (InterruptedException e) {
+        //         throw new RuntimeException(e);
+        //     }
+        // }
     }
 }
