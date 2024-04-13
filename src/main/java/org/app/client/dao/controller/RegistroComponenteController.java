1428434012d8afddb904a21c72101aedcae1b5f7
v-leonel <victor.lima@sptech.school>
2024-04-13 16:29:42 -0300

    Criado controller para RegistroComponente

--- src/main/java/org/app/client/dao/controller/RegistroComponenteController.java
+++ src/main/java/org/app/client/dao/controller/RegistroComponenteController.java
@@ -1,2 +1,16 @@
-package org.app.client.dao.controller;public class RegistroComponenteController {
+package org.app.client.dao.controller;
+
+import com.mysql.cj.jdbc.JdbcConnection;
+import org.app.client.conexao.Conexao;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class RegistroComponenteController {
+    Conexao conexao = new Conexao();
+
+    public void adicionarRegistro(String nome, String valor, Integer fkComponente){
+        JdbcTemplate getConexao = conexao.getJdbcTemplate();
+
+        getConexao.update("Insert into RegistroComponente(nome, valor, fkComponente) VALUES (?,?,?)", nome, valor, fkComponente);
+
+    }
 }
