5c824f5809fa9e6521939bf5a41de2c99297db81
v-leonel <victor.lima@sptech.school>
2024-04-13 16:28:41 -0300

    Criado controller para ComponenteController

--- src/main/java/org/app/client/dao/controller/ComponenteController.java
+++ src/main/java/org/app/client/dao/controller/ComponenteController.java
@@ -1,5 +1,23 @@
 package org.app.client.dao.controller;
 
+import org.app.client.conexao.Conexao;
+import org.app.client.dao.entity.Componente;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class ComponenteController {
-  
+    Conexao conexao = new Conexao();
+
+    public void adicionarComponente(String nome, Integer fkComputador){
+        JdbcTemplate getConexao = conexao.getJdbcTemplate();
+
+        getConexao.update("Insert Into Componente(nome, fkComputador) VALUES(?,?)", nome, fkComputador);
+    }
+
+    public Componente pegarComponente(Integer fkComputador){
+        JdbcTemplate getConexao = conexao.getJdbcTemplate();
+
+        return getConexao.queryForObject("Select * FROM componente JOIN computador ON fkComputador = idComputador WHERE fkComputador = ? ORDER BY idComponente desc LIMIT 1", new BeanPropertyRowMapper<>(Componente.class), fkComputador);
+    }
+
 }
