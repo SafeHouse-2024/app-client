623dce8ecb97f29dc8092539febad5f55bd7f24a
nth-domingues <nathan.piazentin@sptech.school>
2024-05-05 16:00:28 -0300

    Adicionando .JAR individual

--- src/main/java/org/app/client/login/Login.java
+++ src/main/java/org/app/client/login/Login.java
@@ -5,50 +5,57 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.io.Console;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Login {
 
     Conexao conexao = new Conexao();
+    private int tentativasFalhas = 0;
+    private boolean bloqueado = false;
+    private long tempoBloqueio = 0;
 
 
     public String autenticar(Integer fkSistemaOperacional) {
-
         Scanner scanner = new Scanner(System.in);
 
-        Console console = System.console();
-        String senha = "";
-        String sudo = " ";
-
         while (true) {
+            if (bloqueado) {
+                long agora = System.currentTimeMillis();
+                if (agora < tempoBloqueio) {
+                    System.out.println("O site está bloqueado. Tente novamente mais tarde.");
+                    return null; // Ou outra ação, dependendo do contexto
+                } else {
+                    // Reiniciar as tentativas após o tempo de bloqueio expirar
+                    bloqueado = false;
+                    tentativasFalhas = 0;
+                    System.out.println("O bloqueio foi removido. Você pode tentar novamente.");
+                }
+            }
+
             System.out.println("Digite o email: ");
             String email = scanner.nextLine();
             System.out.println("Digite a senha: ");
-            if (console != null){
-                senha = String.valueOf(console.readPassword());
-            } else {
-                senha = scanner.nextLine();
-            }
-            if (fkSistemaOperacional == 2){
-                System.out.println("Digite a senha do sudo: ");
-                if (console != null){
-                    sudo = String.valueOf(console.readPassword());
-                }else {
-                    sudo = scanner.nextLine();
-                }
-            }
+            String senha = scanner.nextLine();
             System.out.println("Digite o código de acesso da máquina");
             String codigoAcesso = scanner.nextLine();
 
             if (autenticar(email, senha, codigoAcesso)) {
                 System.out.println("Usuário autenticado");
-                return codigoAcesso + "#" + sudo;
+                return codigoAcesso;
+            } else {
+                tentativasFalhas++;
+                if (tentativasFalhas >= 3) {
+                    // Bloquear o acesso por 1 minuto
+                    bloqueado = true;
+                    tempoBloqueio = System.currentTimeMillis() + 60000; // 1 minuto em milissegundos
+                    System.out.println("Você excedeu o número de tentativas permitidas. O site está bloqueado por 1 minuto.");
+                } else {
+                    System.out.println("Tentativa de login falhou. Tente novamente.");
+                }
             }
         }
     }
+
 
     private boolean autenticar(String email, String senha, String codigoAcesso) {
         JdbcTemplate getConexao = conexao.getJdbcTemplate();
