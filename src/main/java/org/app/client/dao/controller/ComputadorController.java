130b9261d6d5b01a6fd01e5d96fe4c7abb7fc4bb
v-leonel <victor.lima@sptech.school>
2024-05-18 22:41:23 -0300

    Realizada conexão das classes com SqlServer (Inserts e Updates)

--- src/main/java/org/app/client/dao/controller/ComputadorController.java
+++ src/main/java/org/app/client/dao/controller/ComputadorController.java
@@ -3,6 +3,7 @@ package org.app.client.dao.controller;
 import java.util.List;
 
 import org.app.client.conexao.Conexao;
+import org.app.client.conexao.ConexaoSql;
 import org.app.client.dao.entity.Componente;
 import org.app.client.dao.entity.Computador;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,10 +11,14 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 public class ComputadorController {
   Conexao conexao = new Conexao();
+  ConexaoSql conexaoSql = new ConexaoSql();
 
   public void ativarMaquina(Integer idComputador){
     JdbcTemplate getConexao = conexao.getJdbcTemplate();
-    Integer alteracoes = getConexao.update("UPDATE Computador SET ativo = 'Ativo' WHERE idComputador = ?", idComputador);
+    JdbcTemplate getConexaoSql = conexaoSql.getJdbcTemplate();
+
+    getConexao.update("UPDATE Computador SET ativo = 'Ativo' WHERE idComputador = ?", idComputador);
+    getConexaoSql.update("UPDATE Computador SET ativo = 'Ativo' WHERE idComputador = ?", idComputador);
     System.out.println("Máquina ativada com sucesso!");
   }
 
@@ -26,9 +31,10 @@ public class ComputadorController {
 
   public void inserirSistemaOperacional(Integer fkSistemaOperacional, Computador computador){
       JdbcTemplate getConexao = conexao.getJdbcTemplate();
+      JdbcTemplate getConexaoSql = conexaoSql.getJdbcTemplate();
 
-
-    getConexao.update("INSERT INTO SistemaComputador(fkSistemaOperacional, fkComputador) VALUES (?,?)", fkSistemaOperacional, computador.getIdComputador());
+      getConexaoSql.update("INSERT INTO SistemaComputador(fkSistemaOperacional, fkComputador) VALUES (?,?)", fkSistemaOperacional, computador.getIdComputador());
+      getConexao.update("INSERT INTO SistemaComputador(fkSistemaOperacional, fkComputador) VALUES (?,?)", fkSistemaOperacional, computador.getIdComputador());
   }
 
 }
--- src/main/java/org/app/client/dao/controller/RegistroComponenteController.java
+++ src/main/java/org/app/client/dao/controller/RegistroComponenteController.java
@@ -1,15 +1,19 @@
 package org.app.client.dao.controller;
 
 import org.app.client.conexao.Conexao;
+import org.app.client.conexao.ConexaoSql;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class RegistroComponenteController {
     Conexao conexao = new Conexao();
+    ConexaoSql conexaoSql = new ConexaoSql();
 
     public void adicionarRegistro(String nome, String valor, Integer fkComponente){
         JdbcTemplate getConexao = conexao.getJdbcTemplate();
+        JdbcTemplate getConexaoSql = conexaoSql.getJdbcTemplate();
 
         getConexao.update("INSERT INTO RegistroComponente(nome, valor, fkComponente) VALUES (?,?,?)", nome, valor, fkComponente);
+        getConexaoSql.update("INSERT INTO RegistroComponente(nome, valor, fkComponente) VALUES (?,?,?)", nome, valor, fkComponente);
     }
 
 }
